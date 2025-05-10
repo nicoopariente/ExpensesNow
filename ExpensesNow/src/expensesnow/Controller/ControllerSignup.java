c3553e61e93d65b4d4352fdaa70ec32bbbba6139
nicoopariente <hnpf2019@gmail.com>
2025-05-10 14:52:59 -0300

    Finishing_Login_and_Signup_Logic

---
+++ ExpensesNow/src/expensesnow/Controller/ControllerSignup.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package expensesnow.Controller;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import expensesnow.Views.LogInView;
+ 
+/**
+ *
+ * @author User
+ */
+public class ControllerLogin implements MouseListener {
+    
+    private LogInView login;
+    private ControllerApp controllerApp;
+
+    public ControllerLogin(LogInView login, ControllerApp controller) {
+        this.login = login;
+        this.controllerApp = controller;
+        login.getLoginButton().addMouseListener(this);
+    }
+    
+    
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+        if(e.getSource() == this.login.getLoginButton()){
+            controllerApp.showSignUp();
+        }
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+    
+}
